5feba5231452b670722d19dca9488e461c9f411c
Ittaimann <ittaiam@gmail.com>
2018-01-15 19:11:22 -0800

    testing custome tools and editor elements, may help with fast creating cards

---
+++ FastPaper/Assets/Editor/TestEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(cardScript))]
+public class TestEditor : Editor {
+
+	public override void OnInspectorGUI()
+	{
+		cardScript myTarget = (cardScript)target;
+		DrawDefaultInspector();
+		//myTarget.experience = EditorGUILayout.IntField("Experince",myTarget.experience);
+		EditorGUILayout.LabelField("Level",myTarget.Level.ToString());
+        EditorGUILayout.HelpBox("Testing custome inspector tech, don't be to mindful of me", MessageType.Info);
+		if(GUILayout.Button("huh"))
+		{
+			myTarget.BuildObject();
+		}
+    }
+}
---
+++ FastPaper/Assets/Scripts/cardScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cardScript : MonoBehaviour {
+
+    // Use this for initialization   public int experience;
+    public GameObject obj;
+    public int experience;
+    
+    [SerializeField] public int Level
+    {
+        get{return experience/750;}
+    }
+
+    public void BuildObject()
+    {
+        Instantiate(obj, new Vector3(0,0,0), Quaternion.identity);
+    }
+}
